5bf10d6de5398b7a8b548965e900c51a3a5036a7
sbsends <sbsends@gmail.com>
2018-12-04 20:29:43 -0600

    main: changed chunk size

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package spice
 
 // ChunkSize is the size in bytes of a given encrypted chunk.
 //
-const ChunkSize int = 16384
+const ChunkSize int = 16368
 
 // Overhead ...
 const Overhead int = 16
