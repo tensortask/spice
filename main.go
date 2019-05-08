f22676b0c8d4eb63d88c29da01e29662da7cc6e5
Sam Sendelbach <sbsends@gmail.com>
2019-05-07 23:21:00 -0500

    spice: added main function defaults / pkg description
    moved other default functions out

--- main.go
+++ main.go
@@ -1,53 +1,9 @@
+// Package spice ...
 package spice
 
-import (
-	"crypto/rand"
-	"io"
-
-	"golang.org/x/crypto/nacl/box"
-)
-
 // DefaultChunkSize is the size in bytes of a given encrypted chunk.
-//
 const DefaultChunkSize int = 16368
 
-// DefaultOverhead ...
+// DefaultOverhead is the additional number of bytes added to every
+// sealed frame of data.
 const DefaultOverhead int = 16
-
-// Keypair ...
-type Keypair struct {
-	Public  *[32]byte
-	Private *[32]byte
-}
-
-// SharedKey ...
-func (k *Keypair) SharedKey(peerPublicKey *[32]byte) *[32]byte {
-	sharedKey := new([32]byte)
-	box.Precompute(sharedKey, peerPublicKey, k.Private)
-	return sharedKey
-}
-
-// RandomNonce ...
-func RandomNonce() (*[24]byte, error) {
-	var nonce [24]byte
-	_, err := io.ReadFull(rand.Reader, nonce[:])
-	if err != nil {
-		return nil, err
-	}
-	return &nonce, nil
-}
-
-// RandomKeypair ...
-func RandomKeypair() (Keypair, error) {
-	publicKey, privateKey, err := box.GenerateKey(rand.Reader)
-	if err != nil {
-		return Keypair{}, err
-	}
-	return Keypair{Public: publicKey, Private: privateKey}, nil
-}
-
-// NewKeypair ...
-func NewKeypair(public *[32]byte, private *[32]byte) Keypair {
-	return Keypair{Public: public, Private: private}
-	//
-}
